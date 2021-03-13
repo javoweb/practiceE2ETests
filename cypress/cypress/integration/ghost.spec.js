0c4354018eb7ae7e7c739ee4e420b4cff22afc17
Gonzalo Tixilima <javoweb@msn.com>
2021-03-12 22:52:36 -0500

    refactor cypress spec

--- cypress/cypress/integration/ghost.spec.js
+++ cypress/cypress/integration/ghost.spec.js
@@ -1,109 +1,97 @@
+const credentials = require('../fixtures/loginCredentials.json')
+
 describe('Ghost', () => {
     beforeEach(() => {
-        cy.visit('ghost/#/signin')
-        cy.wait(1500)
+        cy.visitLoginPage()
     })
     context('Login tests', () => {
         it('Wrong Credentials', () => {
-            cy.get('#ember8').type('exam@exam.com')
-            cy.get('#ember10').type('password')
-            cy.get('#ember12 > span').click()
-            cy.get('p.main-error').then(($p) => {
+            cy.fillLogin('user@user.com', 'jdv;nsfnd').click()
+            cy.getLoginError().then(($p) => {
                 expect($p.get(0).innerText).to.include('There is no user with that email address')
             })
         })
 
         it('Login Successful', () => {
-            cy.get('#ember8').type('example@example.com')
-            cy.get('#ember10').type('password')
-            cy.get('#ember12 > span').click()
+            cy.fillLogin(credentials.email, credentials.password).click()
             cy.wait(1000)
             cy.url().should('eq', 'http://localhost:2368/ghost/#/site')
         })
     })
     context('Admin Page Tests', () => {
         beforeEach(() => {
-            cy.get('#ember8').type('example@example.com')
-            cy.get('#ember10').type('password')
-            cy.get('#ember12 > span').click()
+            cy.fillLogin(credentials.email, credentials.password).click()
             cy.wait(1000)
         })
         context('Create Post', () => {
             it('Start Post', () => {
-                cy.get('a[title="New post"').click()
-                cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').clear().type('Test')
-                cy.get('.koenig-editor__editor.__mobiledoc-editor').type('t')
+                cy.goToNewPost()
+                cy.typeTitle('Test')
+                cy.typeContents('t', false)
             })
             it('Continue Post', () => {
-                cy.get('a[href="#/posts/"]').first().click()
-                cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').filter(':visible').first().click({force: true})
-                cy.get('.koenig-editor__editor.__mobiledoc-editor').clear().type('test')
-                cy.get('.ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-outline.gh-publishmenu-trigger').click()
-                cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-                cy.get('.gh-notification-title').then(($title) => {
+                cy.goToPostsPage()
+                cy.clickOnFirstPost()
+                cy.typeContents('test')
+                cy.clickOnPublish()
+                cy.getNotification().then(($title) => {
                     expect($title.get(0).innerText).to.include('Published')
                 })
             })
             it('Start Invalid Post', () => {
-                cy.get('a[title="New post"').click()
-                cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').clear().type('Test')
-                cy.get('.koenig-editor__editor.__mobiledoc-editor').type('t')
+                cy.goToNewPost()
+                cy.typeTitle('Test')
+                cy.typeContents('t', false)
             })
             it('Continue Invalid Post', () => {
-                cy.get('a[href="#/posts/"]').first().click()
-                cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').filter(':visible').first().click({force: true})
-                cy.get('.koenig-editor__editor.__mobiledoc-editor').clear().type('test')
-                cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').clear().type('Testjckdksajsdkjasdjkdfskjbsdfkvbjsdnfvnjsdkjfvbhsbdd;aksdcn;jabsdckjl; klvcn lksndc ;lnxsdavjkasddvjkasdvkjnasjew;jvnw;jvnd;wkldsnvkdnsf;vlnfjdv;sdjfvn;sdjf vnsdjndvnsakdjn ;asnd lansdlkjajsdbkcbasdkjcnj;asdnc;jasndkbhasdjcnadsncasjbvadsf vsdv sdv sdf vsdf gfs fdtgerynhglksdca')
-                cy.get('.ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-outline.gh-publishmenu-trigger').click()
-                cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-                cy.get('.gh-alert-content').then(($title) => {
+                cy.goToPostsPage()
+                cy.clickOnFirstPost()
+                cy.typeContents('test')
+                cy.typeTitle('Testjckdksajsdkjasdjkdfskjbsdfkvbjsdnfvnjsdkjfvbhsbdd;aksdcn;jabsdckjl; klvcn lksndc ;lnxsdavjkasddvjkasdvkjnasjew;jvnw;jvnd;wkldsnvkdnsf;vlnfjdv;sdjfvn;sdjf vnsdjndvnsakdjn ;asnd lansdlkjajsdbkcbasdkjcnj;asdnc;jasndkbhasdjcnadsncasjbvadsf vsdv sdv sdf vsdf gfs fdtgerynhglksdca')
+                cy.clickOnPublish()
+                cy.getAlert().then(($title) => {
                     expect($title.get(0).innerText).to.include('Saving failed')
                 })
             })
         })
         context('Edit Post', () => {
             beforeEach(() => {
-                cy.get('a.ember-view[title="Published"]').click()
-                cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').filter(':visible').first().click({force: true})
+                cy.goToPublishedPage()
+                cy.clickOnFirstPost()
             })
             it('Edit Valid Post', () => {
-                cy.get('.koenig-editor__editor.__mobiledoc-editor').clear().type('edited test')
-                cy.get('.ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-outline.gh-publishmenu-trigger').click()
-                cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-                cy.get('.gh-notification-title').then(($title) => {
+                cy.typeContents('edited test')
+                cy.clickOnPublish()
+                cy.getNotification().then(($title) => {
                     expect($title.get(0).innerText).to.include('Updated')
                 })
             })
             it('Edit Invalid Post', () => {
-                cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').clear().type('Testjckdksajsdkjasdjkdfskjbsdfkvbjsdnfvnjsdkjfvbhsbdd;aksdcn;jabsdckjl; klvcn lksndc ;lnxsdavjkasddvjkasdvkjnasjew;jvnw;jvnd;wkldsnvkdnsf;vlnfjdv;sdjfvn;sdjf vnsdjndvnsakdjn ;asnd lansdlkjajsdbkcbasdkjcnj;asdnc;jasndkbhasdjcnadsncasjbvadsf vsdv sdv sdf vsdf gfs fdtgerynhglksdca')
-                cy.get('.ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-outline.gh-publishmenu-trigger').click()
-                cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-                cy.get('.gh-alert-content').then(($title) => {
+                cy.typeTitle('Testjckdksajsdkjasdjkdfskjbsdfkvbjsdnfvnjsdkjfvbhsbdd;aksdcn;jabsdckjl; klvcn lksndc ;lnxsdavjkasddvjkasdvkjnasjew;jvnw;jvnd;wkldsnvkdnsf;vlnfjdv;sdjfvn;sdjf vnsdjndvnsakdjn ;asnd lansdlkjajsdbkcbasdkjcnj;asdnc;jasndkbhasdjcnadsncasjbvadsf vsdv sdv sdf vsdf gfs fdtgerynhglksdca')
+                cy.clickOnPublish()
+                cy.getAlert().then(($title) => {
                     expect($title.get(0).innerText).to.include('Update failed')
                 })
             })
         })
         context('Delete Post', () => {
             beforeEach(() => {
-                cy.get('a.ember-view[title="Published"]').click()
-                cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').filter(':visible').first().click({force: true})
+                cy.goToPublishedPage()
+                cy.clickOnFirstPost()
             })
             it('Cancel Delete Post', () => {
-                cy.get('button.post-settings').click()
-                cy.get('button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').click()
-                cy.get('button.gh-btn[data-ember-action=""]').last().click()
+                cy.clickOnDelete()
+                cy.clickOnCancel()
             })
             it('Delete Post', () => {
-                cy.get('button.post-settings').click()
-                cy.get('button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').click()
-                cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').first().click()
+                cy.clickOnDelete()
+                cy.clickOnAccept()
                 cy.url().should('eq', 'http://localhost:2368/ghost/#/posts?type=published')
             })
         })
         context('Logout', () => {
             it('Logout Ghost', () => {
-                cy.get('.gh-user-email').click()
-                cy.get('.dropdown-item.user-menu-signout.ember-view').click()
+                cy.clickOnLogout()
                 cy.url().should('eq', 'http://localhost:2368/__/#/signin')
             })
         })
--- cypress/cypress/support/commands.js
+++ cypress/cypress/support/commands.js
@@ -23,3 +23,88 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+
+// Go To Commands
+
+Cypress.Commands.add("goToNewPost", () => {
+    cy.get('a[title="New post"').click()
+})
+Cypress.Commands.add("goToPostsPage", () => {
+    cy.get('a[href="#/posts/"]').first().click()
+})
+
+Cypress.Commands.add("goToPublishedPage", () => {
+    cy.get('a.ember-view[title="Published"]').click()
+})
+
+Cypress.Commands.add("visitLoginPage", () => {
+    cy.visit('ghost/#/signin')
+    cy.wait(1500)
+})
+
+
+// Typing Commands
+
+Cypress.Commands.add("typeTitle", (text) => {
+    cy.get('textarea.gh-editor-title.ember-text-area.gh-input.ember-view').clear().type(text)
+})
+
+Cypress.Commands.add("typeContents", (text, clear = true) => {
+    if (clear) {
+        cy.get('.koenig-editor__editor.__mobiledoc-editor').clear().type(text)
+    } else {
+        cy.get('.koenig-editor__editor.__mobiledoc-editor').type(text)
+    } 
+})
+
+Cypress.Commands.add("fillLogin", (email, password) => {
+    cy.get('#ember8').type(email)
+    cy.get('#ember10').type(password)
+    cy.get('#ember12 > span')
+})
+
+
+// Get Commands
+
+Cypress.Commands.add("getLoginError", () => {
+    cy.get('p.main-error')
+})
+
+Cypress.Commands.add("getAlert", () => {
+    cy.get('.gh-alert-content')
+})
+
+Cypress.Commands.add("getNotification", () => {
+    cy.get('.gh-notification-title')
+})
+
+
+// Click Commands
+
+Cypress.Commands.add("clickOnFirstPost", () => {
+    cy.get('a.ember-view.permalink.gh-list-data.gh-post-list-title').filter(':visible').first().click({force: true})
+})
+
+Cypress.Commands.add("clickOnPublish", () => {
+    cy.get('.ember-view.ember-basic-dropdown-trigger.gh-btn.gh-btn-outline.gh-publishmenu-trigger').click()
+    cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
+})
+
+Cypress.Commands.add("clickOnDelete", () => {
+    cy.get('button.post-settings').click()
+    cy.get('button.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').click()
+})
+
+Cypress.Commands.add("clickOnCancel", () => {
+    cy.get('button.gh-btn[data-ember-action=""]').last().click({force: true})
+})
+
+Cypress.Commands.add("clickOnAccept", () => {
+    cy.get('button.gh-btn.gh-btn-red.gh-btn-icon.ember-view').first().click()
+})
+
+Cypress.Commands.add("clickOnLogout", () => {
+    cy.get('.gh-user-email').click()
+    cy.get('.dropdown-item.user-menu-signout.ember-view').click()
+})
